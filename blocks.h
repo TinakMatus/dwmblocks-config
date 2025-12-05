960333d72dab75c63bb57aaaa8a6f4428a35dc2b
tinakmatus <matustinak1@gmail.com>
2025-12-05 23:32:27 +0100

    added bluethoot to dwmblock with clickable acctions

--- blocks.h
+++ blocks.h
@@ -1,14 +1,23 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/                                        /*Update Interval*/ /*Update Signal*/
+    
+    // 1. Battery
     {"BAT: ", "acpi | awk -F '[ ,%]+' '{print $4 \"%\"}'",       30,                  0},
+
+    // 2. Volume (Signal 10)
     {"VOL: ", "pamixer --get-volume-human",                       0,                   10},
+    
+    // 3. Date
     {"",      "date '+%b %d (%a) %H:%M'",                        60,                  0},
-    // CHANGED: Update interval increased from 5 to 30 seconds
-    {"",      "/home/bread/.local/suckless/bin/sb-wifi",         30,                  5}, 
+
+    // 4. Wi-Fi (Signal 5)
+    {"",      "/home/bread/.local/suckless/bin/sb-wifi",         30,                  5},
+
+    // 5. Bluetooth (Signal 7) - ADD THIS LINE
+    {"",      "/home/bread/.local/suckless/bin/sb-bluetooth",     5,                  7},
 };
 
 // sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
-// CRITICAL FIX 2: Corrected length for " | " is 3 (space, pipe, space).
 static unsigned int delimLen = 3;
